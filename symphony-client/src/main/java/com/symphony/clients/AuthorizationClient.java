9f168530cc9832c0119be313c1ce72e998a767dd
nick.tarsillo <ricklypop@gmail.com>
2017-07-09 18:23:05 -0700

    Added SSL configuration for http client

--- symphony-client/src/main/java/com/symphony/clients/AuthorizationClient.java
+++ symphony-client/src/main/java/com/symphony/clients/AuthorizationClient.java
@@ -11,7 +11,12 @@ import org.symphonyoss.symphony.authenticator.invoker.Configuration;
 
 import java.io.FileInputStream;
 import java.security.KeyStore;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 
@@ -80,9 +85,26 @@ public class AuthorizationClient {
     loadKeyStore(cks, clientKeyStore, clientKeyStorePass);
     loadKeyStore(tks, trustStore, trustStorePass);
 
+    TrustManager[] trustManager = new TrustManager[] {new X509TrustManager() {
+      public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws
+          CertificateException {}
+
+      public void checkServerTrusted(X509Certificate[] arg0, String arg1)
+          throws CertificateException {}
+
+      public X509Certificate[] getAcceptedIssuers() {
+        return new X509Certificate[0];
+      }
+    }};
+
+    SSLContext sslcontext = SSLContext.getInstance("TLS");
+    sslcontext.init(null, trustManager, new java.security.SecureRandom());
+
     httpClient = ClientBuilder.newBuilder()
+        .sslContext(sslcontext)
         .keyStore(cks, clientKeyStorePass.toCharArray())
         .trustStore(tks)
+        .hostnameVerifier((string, sll) -> true)
         .build();
   }
 
